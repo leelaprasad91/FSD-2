b419881d1360cef58b9ca1cf060b7dd2914ae1f8
leelaprasad91 <lpreddy3773@gmail.com>
2026-08-18 14:37:56 +0530

    Delete EXP4 directory

--- EXP4/src/mocks/handlers.js
+++
@@ -1,8 +0,0 @@
-import { http, HttpResponse } from "msw";
-import { initialEvents } from "../data/initialEvents.js";
-
-export const handlers = [
-  http.get("/api/events", () => {
-    return HttpResponse.json(initialEvents);
-  }),
-];
--- EXP4/src/setupTests.js
+++
@@ -1,6 +0,0 @@
-import "@testing-library/jest-dom";
-import { server } from "./mocks/server.js";
-
-beforeAll(() => server.listen({ onUnhandledRequest: "warn" }));
-afterEach(() => server.resetHandlers());
-afterAll(() => server.close());
